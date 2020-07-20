156584ddbb7da8384542ba50d490c9abdf9fca16
trevormh <trevormholloway@gmail.com>
2020-07-20 14:44:48 -0500

    fixed issue with bloodhound causing static urls

--- resources/views/pages/Search/typeahead.blade.php
+++ resources/views/pages/Search/typeahead.blade.php
@@ -5,29 +5,39 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/typeahead.js/0.11.1/typeahead.bundle.min.js"></script>
 
 <div>
-    <form class="typeahead" role="search">
+    <form class="typeahead" role="search" method="GET" action="/">
         <div class="form-group">
-            <label for="search_params">Field:</label>
-            <select name="search_params" id = "search_field">
+            <label for="field">Field:</label>
+            <select name="field" id = "field">
                 <option value ="physician_first_name">Physician First Name</option>
                 <option value ="physician_last_name">Physician Last Name</option>
                 <option value ="total_amount_of_payment_usdollars">Amount</option>
             </select>
             <input type="search" name="q" class="form-control search-input" placeholder="Search" autocomplete="off">
+            <button type= "submit">Submit</button>
         </div>
     </form>
 </div>
 
 <script>
-        jQuery(document).ready(function($) {
+        
+        var searchField = $('#field').val();
+        // $('#field').change(function() {
+        //     searchField = $(this).val());
+        // };
 
-            var searchField = document.getElementById("search_field").value;
+        jQuery(document).ready(function($) {
 
             // Set the Options for "Bloodhound" suggestion engine
             var engine = new Bloodhound({
                 remote: {
-                    url: '/search?field=' + searchField + '&q=%QUERY%',
-                    wildcard: '%QUERY%'
+                    url: '/typeaheadSearch',
+                    prepare : function(query, settings) {
+                        settings.url += '?q=' + query;
+                        // return settings;
+                        settings.url += "&field=" + $('#field').val();
+                        return settings;
+                    }
                 },
                 datumTokenizer: Bloodhound.tokenizers.whitespace('q'),
                 queryTokenizer: Bloodhound.tokenizers.whitespace
@@ -39,20 +49,15 @@
                 minLength: 1
             }, {
                 source: engine.ttAdapter(),
-
-                // This will be appended to "tt-dataset-" to form the class name of the suggestion menu.
-                name: 'usersList',
-
-                // the key from the array we want to display (name,id,email,etc...)
+                display: searchField, 
                 templates: {
-                    empty: [
-                        '<div class="list-group search-results-dropdown"><div class="list-group-item">Nothing found.</div></div>'
-                    ],
+                    
                     header: [
                         '<div class="list-group search-results-dropdown">'
                     ],
                     suggestion: function (data) {
-                        return '<a href=/?field=' + searchField + '&q=' + data[searchField] + ' class="list-group-item">' +  data[searchField] + '</a>'
+                        let fieldName = $('#field').val()
+                        return '<a href=/?field=' + fieldName + '&q=' + data[fieldName] + ' class="list-group-item">' +  data[fieldName] + '</a>'
               }
                 }
             });
